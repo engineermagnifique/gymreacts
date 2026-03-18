a17c179f5aebe9f1645a844844c8d68bce940124
Magnifique <magnifiqueni01@gmail.com>
2026-03-18 13:45:18 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,38 @@
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { BrowserRouter, NavLink} from 'react-router-dom';
 
-function Home() {
-  return <h1>Home Page</h1>;
+const Home=()=>{
+  return (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+  )
+}
+const About=()=>{
+  return (
+  <div>
+    <h1>About Page</h1>
+  </div>
+  )
 }
 
-function Products() {
+const navLinkStyle=({isActive})=>({
+    color:isActive?'red':'blue',
+    textDecoration:isActive?'underline':'none'
+  })
+const App=()=>{
   return (
     <div>
-      <h1>Products Page</h1>
-      <nav style={{ marginBottom: '20px' }}>
-        <Link to="/products/car">Cars</Link> |{" "}
-        <Link to="/products/bike">Bikes</Link>
-      </nav> 
-      <Outlet /> 
+      <BrowserRouter>
+       <nav style={{display:'flex',gap:'20px'}}>
+         <NavLink to="/" style={navLinkStyle}>Home</NavLink>
+         <NavLink to="/about" style={navLinkStyle}>About</NavLink>
+       </nav>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+       </Routes>
+      </BrowserRouter>
     </div>
-  );
-}
-
-function CarProducts() {
-  return (
-    <div>
-      <h2>Cars</h2>
-      <ul>
-        <li>Audi</li>
-        <li>BMW</li>
-        <li>Volvo</li>
-      </ul>
-    </div>
-  );
-}
-
-function BikeProducts() {
-  return (
-    <div>
-      <h2>Bikes</h2>
-      <ul>
-        <li>Yamaha</li>
-        <li>Suzuki</li>
-        <li>Honda</li>
-      </ul>
-    </div>
-  );
-}
-
-function Contact() {
-  return <h1>Contact Page</h1>;
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/products">Products</Link> |{" "}
-        <Link to="/contact">Contact</Link>
-      </nav>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />}>
-          <Route path="car" element={<CarProducts />} />
-          <Route path="bike" element={<BikeProducts />} />
-        </Route>
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  )
 }
 export default App;
